60edae47510c4a27e85fb7db5f57f738a7b3968d
stiga <stiga@imac.local>
2017-04-18 16:34:09 +0530

    Initial Commit

---
+++ SaurabhGitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaurabhGitTutorial
+//
+//  Created by STIGA-IMAC1 on 18/04/17.
+//  Copyright © 2017 STIGA-IMAC1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
